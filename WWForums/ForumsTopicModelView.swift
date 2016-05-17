11c77f9e6eb935224392cf15be99aa6874b94174
ynie88 <ynie.88@gmail.com>
2016-05-17 15:40:52 -0400

    something

---
+++ WWForums/ForumsTopicModelView.swift
@@ -0,0 +1,10 @@
+//
+//  ForumsTopicModelView.swift
+//  WWForums
+//
+//  Created by Yuchen Nie on 5/17/16.
+//  Copyright © 2016 WeddingWire. All rights reserved.
+//
+
+import Foundation
+import WWRequestKit
---
+++ WWForums/ForumsTopicsPresenter.swift
@@ -0,0 +1,10 @@
+//
+//  ForumsTopicsPresenter.swift
+//  WWForums
+//
+//  Created by Yuchen Nie on 5/17/16.
+//  Copyright © 2016 WeddingWire. All rights reserved.
+//
+
+import Foundation
+import WWRequestKit
